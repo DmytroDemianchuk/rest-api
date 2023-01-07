52ca57191c984259e7322380f00ef1c0c68da07d
dmytrodemiancuk <moqitee@gmail.com>
2023-01-07 20:00:57 +0100

    Handler

---
+++ pkg/handler/handler.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"github.com/dmytrodemianchuk/rest-api/pkg/service"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{services: services}
+}
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sing-up", h.singUp)
+		auth.POST("/sing-in", h.singIn)
+	}
+
+	//spiski i ih zadachi
+	api := router.Group("/api")
+	{
+		list := api.Group("/list")
+		{
+			list.POST("/", h.createList)
+			list.GET("/", h.getAllList)
+			list.GET("/:id", h.getListById)
+			list.PUT("/:id", h.updateList)
+			list.DELETE("/:id", h.deleteList)
+
+			{
+				items := list.Group(":id/items")
+				items.POST("/", h.createItem)
+				items.GET("/", h.getAllItem)
+				items.GET("/:item_id", h.getItemById)
+				items.PUT("/:item_id", h.updateItem)
+				items.DELETE("/:item_id", h.deleteItem)
+			}
+		}
+	}
+
+	return router
+}
---
+++ pkg/service/service.go
@@ -0,0 +1,16 @@
+package service
+
+type Authorization interface {
+}
+
+type TodoList interface {
+}
+
+type TodoItem interface {
+}
+
+type Service struct {
+	Authorization
+	TodoList
+	TodoItem
+}
